e50df0b56b7383c74902a9307597a1e10032972a
Aanjneya <aanjneya2@gmail.com>
2025-08-21 09:37:13 +0530

    Inheritance

---
+++ src/Inheritance.java
@@ -0,0 +1,10 @@
+public class Inheritance {
+    protected String address;
+
+    public Inheritance(String address) {
+        this.address = address;
+    }
+    public void show_address() {
+        System.out.println(address);
+    }
+}
---
+++ src/Mymain2.java
@@ -0,0 +1,7 @@
+public class Mymain2 {
+    public static void main(String[] args) {
+        CollegeStudent c1 = new CollegeStudent("KIET",2024001);
+        c1.show_roll();
+        c1.show_address();
+    }
+}
